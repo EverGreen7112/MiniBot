f2274111834fb3abb35fdc39f688a52519244fd1
EverGreen7112 <34937866+EverGreen7112@users.noreply.github.com>
2018-01-19 10:17:29 +0200

    working arm

--- src/org/usfirst/frc/team7112/robot/subsystems/Arm.java
+++ src/org/usfirst/frc/team7112/robot/subsystems/Arm.java
@@ -1,14 +1,11 @@
 package org.usfirst.frc.team7112.robot.subsystems;
 
 import static org.usfirst.frc.team7112.robot.RobotMap.m_Spark1;
-import static org.usfirst.frc.team7112.robot.RobotMap.m_Spark2;
 
 import org.usfirst.frc.team7112.robot.OI;
 import org.usfirst.frc.team7112.robot.commands.ArmClose;
 import org.usfirst.frc.team7112.robot.commands.ArmOpen;
-import org.usfirst.frc.team7112.robot.commands.MoveArm;
 import org.usfirst.frc.team7112.robot.commands.StopMotor;
-import org.usfirst.frc.team7112.robot.commands.TestArm;
 
 import static org.usfirst.frc.team7112.robot.RobotMap.Arm_MicroSwitch;
 
@@ -29,30 +26,24 @@ public class Arm extends Subsystem {
 
 	private static Arm instance;
 	private SpeedController m_Motor1;
-	private SpeedController m_Motor2;
 	private DigitalInput m_MicroSwitch;
 
 	private Arm() {
 		m_Motor1 = new Spark(m_Spark1);
-		m_Motor2 = new Spark(m_Spark2);
 		m_MicroSwitch = new DigitalInput(Arm_MicroSwitch);
+		OI.getInstance().GetXButton().whenPressed(new ArmOpen());
+		OI.getInstance().GetBButton().whenPressed(new ArmClose());
+		OI.getInstance().GetXButton().whenReleased(new StopMotor());
+		OI.getInstance().GetBButton().whenReleased(new StopMotor());
+
 		}
 
 	public void setMotorPower(double power) {
 		m_Motor1.set(power);
-		m_Motor2.set(power);
-	}
-	
-	public void moveArm(){
-			OI.getInstance().GetXButton().whenPressed(new ArmOpen());
-			OI.getInstance().GetBButton().whenPressed(new ArmClose());
-			OI.getInstance().GetXButton().whenReleased(new StopMotor());
-			OI.getInstance().GetBButton().whenReleased(new StopMotor());
 	}
 	
 	public void stopMotor(){
 		m_Motor1.stopMotor();
-		m_Motor2.stopMotor();
 	}
 	
 	public boolean isPressed(){
@@ -68,7 +59,6 @@ public class Arm extends Subsystem {
 	}
 		
     public void initDefaultCommand() {
-    	setDefaultCommand(new MoveArm());
     }
 }
 
